54530affce9b28e686b0b8554eb7203de1755af0
Charline <charlinerobert01480@hotmail.fr>
2021-12-14 11:43:21 +0100

    14-12-21

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,17 @@
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = requires('./routes/user');
+
+mongoose.connect('mongodb+srv://florisette:azerty55@cluster0.z6kvu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
@@ -9,26 +22,11 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
-  });
+  app.use(express.json());
+
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+
+  app.use('api/stuff', stuffRoutes);
+  app.use('api/auth', userRoutes);
 
 module.exports = app;
